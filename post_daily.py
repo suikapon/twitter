41627de1fac812d5e21701c9cfc221956811049d
suikapon <reikotracker@gmail.com>
2026-09-23 11:57:41 +0200

    Update post_daily.py

--- post_daily.py
+++ post_daily.py
@@ -410,7 +410,7 @@ def pick_twitter_content(posted: dict, weights: dict, force_video: bool = False)
     return None
 
 
-
+def build_clients():
     api_key = os.environ["TW_API_KEY"]
     api_secret = os.environ["TW_API_SECRET"]
     access_token = os.environ["TW_ACCESS_TOKEN"]
